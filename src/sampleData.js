cf8f897d72ba77b00d27c1f2a2094efa161b509c
Endzhe Galieva <endzhe91@gmail.com>
2021-07-15 12:17:17 +0300

    First look at state and JSX

---
+++ src/sampleData.js
@@ -0,0 +1,57 @@
+const boards = [
+  {
+    id: 1000,
+    title: 'Course Ideas',
+    background: '#ffad33',
+  },
+  {
+    id: 2000,
+    title: 'House Ideas',
+    background: '#80ccff',
+  },
+  {
+    id: 3000,
+    title: 'Garden Ideas',
+    background: '#ff3300',
+  },
+]
+
+// const lists = [
+//   {
+//     id: 100,
+//     title: 'House ideas',
+//     board: 1000,
+//     cards: [
+//       {
+//         id: 1,
+//         text: 'card 1',
+//       },
+//       {
+//         id: 2,
+//         text: 'card 2',
+//       },
+//     ],
+//   }
+// ]
+//
+// id: 200,
+// title: 'Travel ideas',
+// board: 1000,
+// cards: [
+//   {
+//     id: 11,
+//     text: 'card 11',
+//   },
+// ]
+//
+//
+//
+//     }
+//   }
+// ]
+//
+// const data = {
+//   boards,
+//   lists,
+
+
