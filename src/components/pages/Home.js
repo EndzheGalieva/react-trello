36f47b9cdcca0aefa02362061df80844f9b480d6
Endzhe Galieva <endzhe91@gmail.com>
2021-07-16 12:08:56 +0300

    Controlled components

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import BoardPreview from "../BoardPreview";
+import BoardPreview from '../BoardPreview';
 import PropTypes from 'prop-types';
+import CreateBoardForm from '../CreateBoardForm';
 
 class Home extends React.Component {
-
-newBoard = () => {
-  const board = {
-    title: "Trips to take this year",
-    background: "#80ffaa",
-    createdAt: new Date()
-  }
-  this.props.createNewBoard(board)
-}
-
   render() {
     return (
       <div>
-      <button onClick={this.newBoard}>New board</button>
-      {Object.keys(this.props.boards).map(key => (
+      <CreateBoardForm createNewBoard={this.props.createNewBoard} />
+      <div className="board-preview-wrapper">
+      {
+        Object.keys(this.props.boards).map(key => (
         <BoardPreview
           key={key}
           board={this.props.boards[key]} />
       ))}
+      </div>
       </div>
     )
   }
