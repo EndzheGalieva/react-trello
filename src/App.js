14c008fe9c82f178f5df4be64694ebbbb9a81e77
Endzhe Galieva <endzhe91@gmail.com>
2021-07-16 14:26:51 +0300

    Setting up React router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Board from "./components/Board";
 import data from './sampleData';
 import Home from './components/pages/Home';
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PageNotFound from "./components/pages/PageNotFound";
 class App extends React.Component {
   state = {
     boards: []
@@ -17,9 +18,14 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Home boards={this.state.boards}
-          createNewBoard={this.createNewBoard} />
-          <Board />
+        <BrowserRouter>
+          <Route exact path="/" component={PageNotFound}/>
+          <Route path="/board" component={Board}/>
+          <Route component={PageNotFound} />
+          {/*<Home boards={this.state.boards}*/}
+          {/*   createNewBoard={this.createNewBoard} />*/}
+          {/*<Board />*/}
+        </BrowserRouter>
       </div>
     );
   }
