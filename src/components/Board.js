7a349af3c9eac4231599d6758210056a5076e3ef
Endzhe Galieva <endzhe91@gmail.com>
2021-07-16 13:17:20 +0300

    Uncontrolled components and refs

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,25 +9,49 @@ class Board extends React.Component {
   componentDidMount() {
    this.setState({ currentLists: data.lists })
   }
-  createNewList = () => {
-    const list = {
-      id: Math.random(),
-      title: "My amazing list",
-      board: 300,
-      creadedAt: new Date()
-    }
-    this.setState({ currentLists: [...this.state.currentLists, list]})
+  addBoardInput = React.createRef()
+  createNewList = (e) => {
+    e.preventDefault()
+     const list = {
+       id: Math.random(),
+       title: this.addBoardInput.current.value,
+       board: 300,
+       creadedAt: new Date(),
+       cards: [
+         {
+           id: 1,
+           text: 'Card 1'
+         },
+         {
+           id: 2,
+           text: 'Card 2'
+         }
+       ]
+     }
+     if (list.title) {
+       this.setState({ currentLists: [...this.state.currentLists, list]})
+     }
+     this.addBoardInput.current.value = ''
   }
   render() {
     return (
+      <div>
        <div className="lists-wrapper">
-         <button onClick={this.createNewList}>New list</button>
          {Object.keys(this.state.currentLists).map(key => (
            <List
              key={this.state.currentLists[key].id}
              list={this.state.currentLists[key]} />
          ))}
        </div>
+      <form onSubmit={this.createNewList}
+        className="new-list-wrapper">
+          <input
+          type="text"
+          ref={this.addBoardInput}
+          name="name"
+          placeholder=" + New List" />
+        </form>
+      </div>
     )
   }
 }
