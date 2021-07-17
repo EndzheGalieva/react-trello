7d541c59543f147e7b0f29badccc00bd218d164b
Endzhe Galieva <endzhe91@gmail.com>
2021-07-17 17:29:33 +0300

    Deleting cards and lists from Firebase

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { cardsRef } from '../firebase';
+
 class Card extends React.Component {
+
+  deleteCard = async e => {
+    try{
+       e.preventDefault()
+       const cardId = this.props.data.id
+       const card = await cardsRef.doc(cardId)
+       card.delete()
+    } catch (error) {
+      console.error('Error deleting card: ', error)
+    }
+  }
   render() {
     return (
-      <p>card component</p>
+      <div className="card">
+       <div className="card-body">
+         <p>{this.props.data.text}</p>
+         <span onClick={this.deleteCard}>&times</span>
+      </div>
+      </div>
     )
   }
 }
